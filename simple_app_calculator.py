d1316c5f04500832dba89f2d7b5bb17f68d40e63
Patrick <patrickbbelmonte@gmail.com>
2026-05-04 11:06:37 +0800

    adding two more operations

--- simple_app_calculator.py
+++ simple_app_calculator.py
@@ -2,3 +2,7 @@ def calculate(first_number, second_number, operation):
     """Perform the selected math operation."""
     if operation == 1:
         return first_number + second_number
+    elif operation == 2:
+        return first_number - second_number
+    elif operation == 3:
+        return first_number * second_number
